5f9ccdfeaad5beace933528d9fe3292ba830f8cc
gurpartap singh <gurpartapsingh007@gmail.com>
2016-01-23 16:53:49 +0530

    added Coach Count and Slow Fast status information of trains along with stylesheets

--- train_route.cpp
+++ train_route.cpp
@@ -1,6 +1,5 @@
 #include "train_route.h"
 #include "ui_train_route.h"
-//extern QString selected_train_no;
 extern QString master_train_no;
 extern QString slave_train_no;
 train_route::train_route(QSqlDatabase *emu_database,QWidget *parent) :
@@ -22,18 +21,23 @@ void train_route::current_selected_train_info(bool slave_train)
     QString source_station_name;
     QString destination_station_name;
     QString coach_count;
-    QString ladies_special_train;
     QChar test = 1;
     bool slow=false;
     bool fast=false;
-    QChar ladies_special=0;
-    qDebug() << "Slave train =  " << slave_train;
+
+    /////////////////////////////////////////   STYLESHEETS OF LABELS   //////////////////////////////////
+    ui->coach_count->setStyleSheet("color: rgb(0,0,255)");// BLUE COLOR
+    ui->ladies_special_type->setStyleSheet("color: rgb(0,0,255)");// BLUE COLOR
+    ui->slow_fast_type->setStyleSheet("color: rgb(0,0,255)");// BLUE COLOR
+    ui->source_station_name->setStyleSheet("color: rgb(0,0,255)");// BLUE COLOR
+    ui->destination_station_name->setStyleSheet("color: rgb(0,0,255)");// BLUE COLOR
+    //--------------------------------------------------------------------------------------------------//
     /////////////////////////////////  FINDING SOURCE/DESTINATION STATIONS CODES /////////////////////////
-    QSqlQuery query1("SELECT `src_stn_sno`,`dest_stn_sno` FROM `tbl_TrainNumber` where `train_no`='"+ master_train_no +"'");
-    while(query1.next())
+    QSqlQuery query_find_source_destination_station_codes("SELECT `src_stn_sno`,`dest_stn_sno` FROM `tbl_TrainNumber` where `train_no`='"+ master_train_no +"'");
+    while(query_find_source_destination_station_codes.next())
     {
         source_destination_station_codes.clear();
-        QSqlRecord record = query1.record();
+        QSqlRecord record = query_find_source_destination_station_codes.record();
         for(int i=0; i < record.count(); i++)
             source_destination_station_codes << record.value(i).toString();
     }
@@ -56,33 +60,46 @@ void train_route::current_selected_train_info(bool slave_train)
     //////////////////////////// FIND LADIES_SPECIAL_STATUS OF CURRENT TRAIN ///////////////////
     if (slave_train)
     {
-        qDebug() << slave_train_no;
         QSqlQuery query_ladies_train_status("SELECT `ladies_trn_status` FROM `tbl_slave_route` where `train_number`='"+ slave_train_no + "'");
+        QSqlQuery query_slow_fast_status("SELECT `train_status` FROM `tbl_slave_route` where `train_number`='"+ slave_train_no + "'");
         query_ladies_train_status.next();
-        ladies_special_train = query_ladies_train_status.value(0).toString();
-        ladies_special = ladies_special_train.at(0);
-        if(ladies_special == 1)
+        query_slow_fast_status.next();
+        if(query_ladies_train_status.value(0).toString().at(0) == 1)
         {
             ui->ladies_special_type->setText("Yes");
-         qDebug() << "This is ladies_special Train";
         }
         else
         {
-        qDebug() << "This is not ladies_special train";
-        ui->ladies_special_type->setText("No");
+            ui->ladies_special_type->setText("No");
         }
-        //ladies_special_train.append('\0');
-        //qDebug() << check.contains(ladies_special_train.at(0));
-        //pData = new char(ladies_special_train.size());
-       // memset(pData,ladies_special_train.size(),'\0');
-        //memcpy(pData, ladies_special_train.data(),ladies_special_train.size());
-        //qDebug() << ladies_special_train.at(1);
-        // qDebug() << ladies_special_train.at(2);
-        //qDebug() << ladies_special_train->size();
-        //qDebug() << "TSET BIT " << ladies_special_train->testBit(0);
-        //qDebug() << "LADIES SPECIAL TRAIN -->" <<  query_ladies_train_status.value(0).toInt();
-        //}
+        if(query_slow_fast_status.value(0).toString() == "S")
+            ui->slow_fast_type->setText("Slow");
+        else
+            ui->slow_fast_type->setText("Fast");
     }
+    else
+    {
+        QSqlQuery query_ladies_train_status("SELECT `ladies_train_status` FROM `tbl_TrainNumber` where `train_no`='"+ master_train_no + "'");
+        QSqlQuery query_slow_fast_status("SELECT `train_status` FROM `tbl_slave_route` where `train_number`='"+ master_train_no + "'");
+        query_ladies_train_status.next();
+        query_slow_fast_status.next();
+        if(query_ladies_train_status.value(0).toString().at(0) == 1)
+        {
+            ui->ladies_special_type->setText("Yes");
+        }
+        else
+        {
+            ui->ladies_special_type->setText("No");
+        }
+        if(query_slow_fast_status.value(0).toString() == "S")
+            ui->slow_fast_type->setText("Slow");
+        else
+            ui->slow_fast_type->setText("Fast");
+    }
+    QSqlQuery query_find_coach_count("SELECT `no_of_coaches` FROM `tbl_TrainNumber` where `train_no`='"+ master_train_no + "'");
+    query_find_coach_count.next();
+    qDebug() << query_find_coach_count.value(0).toString();
+    ui->coach_count->setText(query_find_coach_count.value(0).toString());
 }
 
 
